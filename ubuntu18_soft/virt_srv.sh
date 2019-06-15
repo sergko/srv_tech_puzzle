fa1712b5a8124739bad4fb2023de29b832070061
Sergey Kovbyk <kovbyk@gmail.com>
2019-06-15 18:57:28 +0300

    added ubuntu_srv shell install start script

---
+++ ubuntu18_soft/virt_srv.sh
@@ -0,0 +1,13 @@
+apt install -y vagrant virtualbox docker.io
+#Vagrant:
+#* Unknown configuration section 'disksize'.
+#* Unknown configuration section 'vbguest'.
+#FIX:
+#vagrant-disksize (0.1.3, global)
+#vagrant-persistent-storage (0.0.45, global)
+#vagrant-triggers (0.5.3, global)
+#vagrant-vbguest (0.17.2, global)
+#vagrant plugin install vagrant-persistent-storage
+#vagrant plugin install vagrant-disksize
+#vagrant plugin install vagrant-triggers
+#vagrant plugin install vagrant-vbguest
